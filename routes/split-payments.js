a7f5403e9d92108db9be56f7420547cac76ad24b
odikanwa <odikanwamichael@gmail.com>
2022-07-21 13:35:24 +0100

    second commit

--- routes/split-payments.js
+++ routes/split-payments.js
@@ -1,149 +1,67 @@
 import express from 'express';
+import paymentData from '../payments.json' assert{type:'json'}
 
 // const app = express();
 
 const router = express.Router();
 
-
-// Consider using stringify to convert to js from db instead of duplicating
-const payments = [
-    {
-        ID: 1308,
-        Amount: 12580,
-        Currency: "NGN",
-        CustomerEmail: "anon8@customers.io",
-        SplitInfo: [
-            {
-                SplitType: "FLAT",
-                SplitValue: 45,
-                SplitEntityId: "LNPYACC0019"
-            },
-            {
-                SplitType: "RATIO",
-                SplitValue: 3,
-                SplitEntityId: "LNPYACC0011"
-            },
-            {
-                SplitType: "PERCENTAGE",
-                SplitValue: 3,
-                SplitEntityId: "LNPYACC0015"
-            }
-        ]
-    }
-]
-
-
 // All routes here starting with '/split-payments'
 router.get('/compute', (req, res) => {
-    res.send(payments);
+    res.send(paymentData);
 })
 
 router.post('/compute', (req, res) => {
+
     //Get request body
     const request = req.body;
-    // console.log(request)
     const splitInfo = request.SplitInfo;
-    // console.log(splitInfo)
-    //Calculate FLAT, PERCENTAGE & RATIO (in order)
-      //     for(var i = 0; i < payments.length; i++) {
 
     const state = {
-        balance: request.Amount,
+        balance: request.Amount
     }
+
+    const ratios = splitInfo.filter((obj) => obj.SplitType == "RATIO")
+    
+    //Sum ratio values of splitType=RATIO
+    const sumOfRatios = ratios.reduce((a, b) => {
+        return (a + b.SplitValue);
+        }, 0);
+        console.log(sumOfRatios)
             
+    //Loop for splitType=FLAT
     for(var i = 0; i < splitInfo.length; i++) {
-       
         if (splitInfo[i].SplitType == "FLAT") {
             console.log(state.balance, splitInfo[i].SplitType, splitInfo[i].SplitValue, splitInfo[i].SplitEntityId);
             const amount = splitInfo[i].SplitValue;
-            // amount.push(splitValue)
-            // console.log(amount)
             let newBalance = state.balance - amount;
             state.balance = newBalance;
             console.log(state.balance, amount);
         }
     }
+    //Loop for splitType = PERCENTAGE
     for(var i = 0; i < splitInfo.length; i++) {
         if (splitInfo[i].SplitType == "PERCENTAGE") {
             console.log(state.balance, splitInfo[i].SplitType, splitInfo[i].SplitValue, splitInfo[i].SplitEntityId);
             const amount = (splitInfo[i].SplitValue/100) * state.balance;
-            // amount.push(splitValue)
-            // console.log(amount)
             let newBalance = state.balance - amount;
             state.balance = newBalance;
             console.log(state.balance, amount);
+         
         }
     }
-    
 
-    // if (splitInfo.SplitType == "RATIO") {
-    //     const sum = splitInfo.reduce((accumulator, object) => {
-    //         return accumulator + object.SplitValue;
-    //     }, 0);
-    //     console.log(sum)
-    // }
-     
-    // splitInfo.forEach(element => {
-    //     sum += element.SplitValue
+    //Loop for Split Type = RATIO
+    ratios.forEach(item => {
+        if (item.SplitType == "RATIO") {
+            console.log(state.balance, item.SplitType, item.SplitValue, item.SplitEntityId);
+            let openingBalance = state.balance
+            const amount = (item.SplitValue/sumOfRatios) * openingBalance;
+            let newBalance = openingBalance - amount;
+            console.log(newBalance, amount);
+        }
         
-    // });
+    });
 
-    for(var i = 0; i < splitInfo.length; i++) {
-        if (splitInfo[i].SplitType == "RATIO") {
-            let sumOfRatio = 0
-            sumOfRatio += splitInfo[i].SplitValue;
-            console.log(sumOfRatio)
-            console.log(state.balance, splitInfo[i].SplitType, splitInfo[i].SplitValue, splitInfo[i].SplitEntityId);
-
-            // const sum = splitInfo.reduce((accumulator, object) => {
-            //     return (accumulator + object.SplitValue) && object.SplitType == "RATIO";
-            // }, 0);
-            // console.log(sum)
-            
-            // const totalRatio = []
-            // totalRatio.push(splitInfo[i].SplitValue)
-            // const amount =
-            // console.log(totalRatio)
-            // // amount.push(splitValue)
-            // // console.log(amount)
-            // let newBalance = state.balance - amount;
-            // state.balance = newBalance;
-            // console.log(state.balance);
-        }
-    }
-
-    console.log(
-        [10, 20, 30, 40].reduce((a, b) => a + b, 0)
-      )
- 
-
-    // const paymentDetails = splitInfo => {
-    //     console.log('look')
-
-    //     const state = {
-    //         balance: request.Amount,
-            
-    //     }
-    //     console.log(state.balance)
-    //     console.log(request.SplitInfo)
-    //     for(var i = 0; i < payments.length; i++) {
-            
-    //         if (payments[i].SplitType == "FLAT") {
-    //             return
-    //             console.log('Mike')
-    //             // console.log(state.balance, payments[i].SplitType, payments[i].SplitValue, payments[i].SplitEntityId);
-    //             // const amount = payments[i].SplitValue;
-    //             // // amount.push(splitValue)
-    //             // // console.log(amount)
-    //             // let newBalance = state.balance - amount;
-    //             // state.balance = newBalance;
-    //             // console.log(state.balance);
-    //         }
-    //     }
-    // }
-
-
-    //return object
     const payments = {
         ID: request.ID,
         Balance: 0,
